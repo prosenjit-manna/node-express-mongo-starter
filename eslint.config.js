2240eb8e78c957ac7dadc6f46a47d299f2bc961f
Prosenjit Manna <prosenjit@itobuz.com>
2025-01-24 20:16:58 +0530

    eslint configured

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import { includeIgnoreFile } from "@eslint/compat";
+
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  includeIgnoreFile(gitignorePath),
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  {
+    files: ["src/**/*.js"],
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+  },
+];
