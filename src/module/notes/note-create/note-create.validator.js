0d8c92dccdc7801905440dfeed9fc28328c657b8
Prosenjit Manna <prosenjit@itobuz.com>
2025-01-27 20:48:05 +0530

    fixed: note create validator

--- src/module/notes/note-create/note-create.validator.js
+++ src/module/notes/note-create/note-create.validator.js
@@ -6,17 +6,16 @@ import sanitizeHtml from 'sanitize-html';
 const noteSchema = z.object({
   title: z.string().nonempty({ message: "Title is required" }),
   content: z.string().nonempty({ message: "Content is required" }).transform((val) => sanitizeHtml(val)),
-});
+}).strict();
 
 export function noteCreateValidator(req, res, next) {
   const result = noteSchema.safeParse(req.body);
-
   if (!result.success) {
-    const errors = result.error.errors.map((err) => ({ message: err.message }));
-    return res.status(400).send(errors);
+    return res.status(400).json({ errors: result.error.errors.map(err => { 
+      return { field: err.path.join('.'), message: err.message };
+     }) });
   }
-
   req.body = result.data;
-
+  
   next();
 }
